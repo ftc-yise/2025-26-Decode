5fc5e82caffb5481b361b436297bacda221df437
allly <allysmith64000@gmail.com>
2026-04-11 08:47:35 -0600

    auto work 4/11 autoClosePark pathed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoClosePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoClosePark.java
@@ -1,75 +1,72 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
-
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.yise.Turret;
 import org.firstinspires.ftc.teamcode.yise.Parameters;
-
 import java.util.Objects;
-
 
 @Config
 @Autonomous(name = "autoClosePark", group = "Linear Opmode")
 
 public class autoClosePark extends LinearOpMode {
     // default alliance is red
-    public String alliance = "RED";
+    public Turret.turretAlliance alliance = Turret.turretAlliance.RED;
     // this will hold the trajectoryAction we select based on alliance color
     public Action trajectoryActionChosen;
     public DcMotor intake;
-
+    public Pose2d initialPose;
+    public Action traj_1 = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        // default alliance is red, only overwrite if blue was chosen in game values
-        if (Parameters.allianceColor == Parameters.Color.BLUE) {
-            alliance = "BLUE";
+        if (Parameters.allianceColor == Parameters.Color.RED) {
+            alliance = Turret.turretAlliance.RED;
+        } else if (Parameters.allianceColor == Parameters.Color.BLUE) {
+            alliance = Turret.turretAlliance.BLUE;
         }
 
-        // instantiate drive class (MecanumDrive) at a particular pose.
         Pose2d initialPose = null;
-        if (Objects.equals(alliance, "RED")) {
-            initialPose = new Pose2d(53, -50, Math.toRadians(0));
-        }else if (Objects.equals(alliance, "BLUE")) {
-            initialPose = new Pose2d(-53, 50, Math.toRadians(0));
-        }
-        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
-        //Intake intake = new Intake(hardwareMap);
-        intake = hardwareMap.get(DcMotor.class, "intake");
 
-        // we build our trajectories during initialization to avoid wasting time during auto
-        // tab one is for red
+        if (Objects.equals(alliance, Turret.turretAlliance.RED)) {
+            initialPose = new Pose2d(65, 43, Math.toRadians(90));
+        }else if (Objects.equals(alliance, Turret.turretAlliance.BLUE)) {
+            initialPose = new Pose2d(65, -43, Math.toRadians(270));
+        }
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        drive.localizer.setPose(initialPose);
+
+        //red side
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-                //shoot 3 balls
-                .strafeTo(new Vector2d(53, -40));
-        // tab two is for blue
+                .strafeTo(new Vector2d(57,38));
+
+        //blue side now
         TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
-                //shoot 3 balls
-                .strafeTo(new Vector2d(-53, 40));
+                .strafeTo(new Vector2d(57,-38));
 
         waitForStart();
         if (isStopRequested()) return;
 
         // set our trajectoryAction based on alliance color
-        if (Objects.equals(alliance, "RED")) {
-            trajectoryActionChosen = tab1.build();
-        } else if (Objects.equals(alliance, "BLUE")) {
-            trajectoryActionChosen = tab2.build();
+        if (alliance == Turret.turretAlliance.RED) {
+            traj_1 = tab1.build();
+        } else if (alliance == Turret.turretAlliance.BLUE) {
+            traj_1 = tab2.build();
         }
 
-        Actions.runBlocking(trajectoryActionChosen);
+        //shoot 3 balls
+        Actions.runBlocking(traj_1);//park
+
     }
 }
