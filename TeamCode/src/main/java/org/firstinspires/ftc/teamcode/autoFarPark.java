5bd4e2e9f0e922c3c6e2a6ad530c060a59408e24
allly <allysmith64000@gmail.com>
2026-04-11 09:31:32 -0600

    auto work 4/11 autoFarPark pathed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoFarPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoFarPark.java
@@ -9,7 +9,9 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.yise.Turret;
 import org.firstinspires.ftc.teamcode.yise.Parameters;
 import java.util.Objects;
 
@@ -18,27 +20,28 @@ import java.util.Objects;
 
 public class autoFarPark extends LinearOpMode {
     // default alliance is red
-    public String alliance = "RED";
+    public Turret.turretAlliance alliance = Turret.turretAlliance.RED;
     // this will hold the trajectoryAction we select based on alliance color
     public Action trajectoryActionChosen;
     public DcMotor intake;
     public Pose2d initialPose;
+    public Action traj_1 = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         if (Parameters.allianceColor == Parameters.Color.RED) {
-            alliance = "RED";
+            alliance = Turret.turretAlliance.RED;
         } else if (Parameters.allianceColor == Parameters.Color.BLUE) {
-            alliance = "BLUE";
+            alliance = Turret.turretAlliance.BLUE;
         }
 
         Pose2d initialPose = null;
 
-        if (Objects.equals(alliance, "RED")) {
-            initialPose = new Pose2d(19, 65, Math.toRadians(90));
-        }else if (Objects.equals(alliance, "BLUE")) {
-            initialPose = new Pose2d(19, -65, Math.toRadians(270));
+        if (Objects.equals(alliance, Turret.turretAlliance.RED)) {
+            initialPose = new Pose2d(65, 19, Math.toRadians(90));
+        }else if (Objects.equals(alliance, Turret.turretAlliance.BLUE)) {
+            initialPose = new Pose2d(65, -19, Math.toRadians(270));
         }
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
@@ -46,38 +49,24 @@ public class autoFarPark extends LinearOpMode {
 
         //red side
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-                .strafeTo(new Vector2d(35,36));
-
+                .strafeTo(new Vector2d(62,38));
 
         //blue side now
         TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
-                .strafeTo(new Vector2d(35,-36));
-
-
+                .strafeTo(new Vector2d(62,-38));
 
         waitForStart();
         if (isStopRequested()) return;
 
         // set our trajectoryAction based on alliance color
-        if (Objects.equals(alliance, "RED")) {
-            trajectoryActionChosen = tab1.build();
-
-        } else if (Objects.equals(alliance, "BLUE")) {
-            trajectoryActionChosen = tab2.build();
-
+        if (alliance == Turret.turretAlliance.RED) {
+            traj_1 = tab1.build();
+        } else if (alliance == Turret.turretAlliance.BLUE) {
+            traj_1 = tab2.build();
         }
 
-        Action traj_1 = tab1.build();
-        Action traj_2 = tab2.build();
-
-        //red side
         //shoot 3 balls
         Actions.runBlocking(traj_1);//park
-
-
-        //blue side
-        //shoot 3 balls
-        Actions.runBlocking(traj_2);//park
 
     }
 }
